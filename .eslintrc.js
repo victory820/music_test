7524b3ba1b1206c4dea660d523861f54b1f91022
victor <victor.chen@geekbang.org>
2019-07-25 06:54:47 +0800

    init project

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,9 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 'eol-last': 0, // 文件内容最后一行是否有空行
+    // 'space-before-function-paren': 0 // 方法左侧括号是否要加空格
   },
   parserOptions: {
     parser: 'babel-eslint'
--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import './common/stylus/index.styl'
+import fastclick from 'fastclick'
 
+fastclick.attach(document.body)
 Vue.config.productionTip = false
 
 new Vue({
